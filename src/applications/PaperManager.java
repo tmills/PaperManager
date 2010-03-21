8c9ba152a6cb43a028bb78124541473c95beef92
Tim Miller <tim.miller@gmail.com>
2010-03-21 08:35:23 -0500

    Tag viewing/editing working.  Added copyright notice and license (GPLv3).

--- src/applications/PaperManager.java
+++ src/applications/PaperManager.java
@@ -1,18 +1,46 @@
+/*
+ * Copyright 2010 Tim Miller
+ * This file is part of PaperManager
+ * PaperManager is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 package applications;
 
-
+import javax.swing.DefaultListModel;
+import javax.swing.DefaultListSelectionModel;
 import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JList;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JSplitPane;
 import javax.swing.JTable;
+import javax.swing.JTextArea;
+import javax.swing.JLabel;
 import javax.swing.JToolBar;
+import javax.swing.ListModel;
+import javax.swing.event.DocumentEvent;
+import javax.swing.event.DocumentListener;
+import javax.swing.event.ListSelectionEvent;
 import javax.swing.table.AbstractTableModel;
+import javax.swing.table.TableModel;
 
 import papers.*;
+import tags.Tag;
 
 import java.awt.BorderLayout;
 import java.awt.Dimension;
@@ -20,16 +48,19 @@ import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.Set;
 
-/** 
- * This is built out of the TableDemo on the swing tutorial site.
+/*
+ * This is started based on the TableDemo on the swing tutorial site.
+ * TODO (High) Fix exception when removing item
+ * TODO (High) Related?^ Get remove working (saving)
  * TODO (High) Add importation of bibtex files
  * TODO Add exportation of bibtex files
- * TODO (HIGH) Add tag editor pane
- * TODO (HIGH) Add summary editor pane
- * TODO Separate pane for unclassified PDFs
+ * TODO Separate "card" (?) for unclassified PDFs
  * TODO Auto-scan dropbox directory for new pdfs
  * TODO Link to PDFs
+ * TODO (low) Make division between gui elements thicker (prettier)
+ * TODO (low) Make "tag add" button put you in new tag editing mode (no double click required) 
  */
 
 public class PaperManager extends JPanel implements ActionListener{
@@ -38,12 +69,16 @@ public class PaperManager extends JPanel implements ActionListener{
 	private static final String RM_CMD = "RM";
 	private static final String IMP_CMD = "IMPORT";
 	private static final String EXP_CMD = "EXPORT";
+	private static final String WRITE_CMD = "WRITE";
+	private static final String ADD_TAG_CMD = "ADD_TAG";
 	private static final String EXIT = "EXIT";
 	private ArrayList<Paper> paperList=null;
 	PaperFileWriter writer = null;
 	PaperFileReader pfr = null;
 	JTable table;
-	MyTableModel tModel;
+	JTextArea summaryBox=null;
+	RefTableModel tModel;
+	TagTableModel tagModel;
 	
 	public PaperManager(String fn) {
 		
@@ -53,7 +88,7 @@ public class PaperManager extends JPanel implements ActionListener{
 		pfr = new PaperFileReader(fn);
 		paperList = pfr.readFile();
 		writer = new PaperFileWriter(fn);
-		tModel = new MyTableModel();
+		tModel = new RefTableModel();
 		
 		// add toolbar and buttons
 		JToolBar toolbar = new JToolBar("Tool buttons");
@@ -70,16 +105,52 @@ public class PaperManager extends JPanel implements ActionListener{
 		toolbar.add(button);
 
 		// add table
-		table = new JTable(tModel);
-		table.setPreferredScrollableViewportSize(new Dimension(500, 70));
-		table.setFillsViewportHeight(true);
+		table = new RefTable(tModel);
 
 		//Create the scroll pane and add the table to it.
 		JScrollPane scrollPane = new JScrollPane(table);
-
+		
+		// create the editing box for summaries
+		// pane containing text area and button for saving...
+		summaryBox = new JTextArea(1,20);
+		summaryBox.setLineWrap(true);
+		summaryBox.setWrapStyleWord(true);
+		JScrollPane summaryPane = new JScrollPane(summaryBox);
+		JPanel summaryPanel = new JPanel(new BorderLayout());
+		summaryPanel.add(new JLabel("Summary"), BorderLayout.NORTH);
+		summaryPanel.add(summaryPane, BorderLayout.CENTER);
+		JButton writeButton = new JButton();
+		writeButton.setText("Save Summary");
+		writeButton.addActionListener(this);
+		writeButton.setActionCommand(WRITE_CMD);
+		summaryPanel.add(writeButton, BorderLayout.SOUTH);
+		summaryPanel.setPreferredSize(new Dimension(200, 300));
+		
+		tagModel = new TagTableModel();
+		JTable tagList = new JTable(tagModel);
+		JPanel tagPanel = new JPanel(new BorderLayout());
+		JToolBar tagTools = new JToolBar("Tag Tools");
+		JButton addTagButton = new JButton();
+		addTagButton.setText(" + ");
+		addTagButton.addActionListener(this);
+		addTagButton.setActionCommand(ADD_TAG_CMD);
+		tagTools.add(addTagButton);
+		tagPanel.add(new JLabel("Tag Editor"), BorderLayout.NORTH);
+		tagPanel.add(new JScrollPane(tagList), BorderLayout.CENTER);
+		tagPanel.add(tagTools, BorderLayout.SOUTH);
+		tagPanel.setPreferredSize(new Dimension(200, 300));
+		
+		// make a split pane?
+		JSplitPane sidePanel = new JSplitPane(JSplitPane.VERTICAL_SPLIT, summaryPanel, tagPanel); 
+//new GridLayout(2,1));
+//		sidePanel.add(summaryPanel, "1");
+//		sidePanel.add(tagPanel, "2");
+		
+		
 		//Add the scroll pane to this panel.
 		add(toolbar, BorderLayout.NORTH);
 		add(scrollPane, BorderLayout.CENTER);
+		add(sidePanel, BorderLayout.EAST);
 	}
 
 	@Override
@@ -101,6 +172,16 @@ public class PaperManager extends JPanel implements ActionListener{
 			System.err.println("Import command triggered");
 		}else if(arg0.getActionCommand().equals(EXP_CMD)){
 			System.err.println("Export command triggered");
+		}else if(arg0.getActionCommand().equals(WRITE_CMD)){
+			System.err.println("Write command triggered");
+			int row = table.getSelectedRow();
+			Paper selPaper = paperList.get(row);
+			selPaper.setField("summary", summaryBox.getText());
+			writer.writeFile(paperList);
+		}else if(arg0.getActionCommand().equals(ADD_TAG_CMD)){
+			System.err.println("Add tag command triggered");
+			int row = table.getSelectedRow();
+			tagModel.addTag(row);
 		}else if(arg0.getActionCommand().equals(EXIT)){
 			System.err.println("Exit pressed!");
 			System.exit(0);
@@ -109,7 +190,24 @@ public class PaperManager extends JPanel implements ActionListener{
 		}
 	}
 	
-	class MyTableModel extends AbstractTableModel{
+	class RefTable extends JTable{
+		public RefTable(TableModel tm){
+			super(tm);
+		}
+		@Override
+		public void valueChanged(ListSelectionEvent e) {
+			super.valueChanged(e);
+			if(!e.getValueIsAdjusting()){
+				DefaultListSelectionModel dlsm = (DefaultListSelectionModel) e.getSource();
+				int row = dlsm.getAnchorSelectionIndex();
+				Paper curr = paperList.get(row);
+				summaryBox.setText(curr.getField("summary"));
+				tagModel.setData(curr.getTags());
+			}
+		}
+	}
+	
+	class RefTableModel extends AbstractTableModel{
 		private int LABEL_COL;
 		private int TYPE_COL;
 		private int VENUE_COL;
@@ -122,7 +220,7 @@ public class PaperManager extends JPanel implements ActionListener{
 				"PDF?"};
 		private Object[][] data = null;
 
-		public MyTableModel(){
+		public RefTableModel(){
 			loadData();
 		}
 		
@@ -151,13 +249,6 @@ public class PaperManager extends JPanel implements ActionListener{
 		 * data can change.
 		 */
 		public void setValueAt(Object value, int row, int col) {
-			if (DEBUG) {
-				System.out.println("Setting value at " + row + "," + col
-						+ " to " + value
-						+ " (an instance of "
-						+ value.getClass() + ")");
-			}
-
 			data[row][col] = value;
 			//if(col == 0 || col == 1 || col == 4){
 			if(col == LABEL_COL){
@@ -171,11 +262,6 @@ public class PaperManager extends JPanel implements ActionListener{
 			}
 			writer.writeFile(paperList);
 			fireTableCellUpdated(row, col);
-
-			if (DEBUG) {
-				System.out.println("New value of data:");
-				printDebugData();
-			}
 		}
 		
 		public int getColumnCount() {
@@ -214,23 +300,61 @@ public class PaperManager extends JPanel implements ActionListener{
 			if(col == PDF_COL) return false;
 			return true;
 		}
-
-
-		private void printDebugData() {
-			int numRows = getRowCount();
-			int numCols = getColumnCount();
-
-			for (int i=0; i < numRows; i++) {
-				System.out.print("    row " + i + ":");
-				for (int j=0; j < numCols; j++) {
-					System.out.print("  " + data[i][j]);
-				}
-				System.out.println();
-			}
-			System.out.println("--------------------------");
-		}
 	}
 
+	class TagTableModel extends AbstractTableModel{
+		String[] columnNames = {"Current Tags"};
+		private Object[][] data = null;
+		Set<Tag> tags=null;
+		
+		public void setData(Set<Tag> t){
+			tags = t;
+			data = new Object[tags.size()][1];
+			Object[] temp = tags.toArray();
+			for(int i = 0; i < temp.length; i++){
+				data[i][0] = temp[i];
+			}
+			fireTableDataChanged();
+		}
+		
+		public void addTag(int row) {
+			paperList.get(row).addTag("*NEW* Double click to edit");
+			setData(paperList.get(row).getTags());
+		}
+
+		public void setValueAt(Object newData, int row, int col){
+//			System.err.println("Setting row " + row + " to " + newData);
+			Tag oldTag = (Tag) data[row][0];
+			System.err.println("Old tag: " + oldTag);
+			oldTag.setTag(newData.toString());
+			data[row][0] = oldTag;
+			writer.writeFile(paperList);
+		}
+		
+		@Override
+		public int getColumnCount() {
+			return 1;
+		}
+
+		@Override
+		public int getRowCount() {
+			return tags == null ? 0 : tags.size();
+		}
+
+		@Override
+		public Object getValueAt(int rowIndex, int columnIndex) {
+			return data[rowIndex][columnIndex];
+		}
+		
+		public boolean isCellEditable(int row, int col){
+			return true;
+		}
+		
+		public String getColumnName(int col) {
+			return columnNames[0];
+		}
+	}
+	
 	/**
 	 * Create the GUI and show it.  For thread safety,
 	 * this method should be invoked from the
@@ -248,7 +372,7 @@ public class PaperManager extends JPanel implements ActionListener{
 		JMenu menu = new JMenu("File");
 		JMenuItem mi1 = new JMenuItem("Import .bib file (unsupported)");
 		JMenuItem mi2 = new JMenuItem("Export .bib file (unsupported)");
-		JMenuItem miLast = new JMenuItem("Exit (unsupported)");
+		JMenuItem miLast = new JMenuItem("Exit");
 		miLast.setActionCommand(EXIT);
 		miLast.addActionListener(newContentPane);
 		menu.add(mi1);
--- src/papers/Paper.java
+++ src/papers/Paper.java
@@ -1,9 +1,26 @@
+/*
+ * Copyright 2010 Tim Miller
+ * This file is part of PaperManager
+ * PaperManager is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 package papers;
 
 import java.io.File;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.LinkedHashSet;
+import java.util.Set;
 
 import tags.Tag;
 
@@ -36,6 +53,10 @@ public class Paper {
 	
 	public void addTag(String t){
 		tags.add(new Tag(t));
+	}
+	
+	public Set<Tag> getTags(){
+		return tags;
 	}
 	
 	public void setField(String key, String val){
